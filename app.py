88e99c207cc7d55eae68016f66f12aa33fe532c0
hemanthghs <ghshemanth@gmail.com>
2023-03-15 22:15:25 +0530

    Added login and signup pages

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, g, session, redirect, request, render_template, url_for, send_file
 import os
+import pymongo
+
 
 app = Flask(__name__)
 
@@ -9,6 +11,13 @@ app.secret_key = os.urandom(24)
 def index():
     return "Test"
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 if __name__ == "__main__":
     app.run()
